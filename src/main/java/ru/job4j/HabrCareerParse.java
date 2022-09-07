c8b5fdfca2a187cc9875ce47b2421db79e257b43
vmyaskovskiy <slavav1977@yandex.ru>
2022-09-07 12:22:10 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/HabrCareerParse.java
+++ src/main/java/ru/job4j/HabrCareerParse.java
@@ -7,15 +7,36 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
+    private String retrieveDescription(String link) throws IOException {
+        String vacancyDescription = " ";
+        ArrayList<String> vacancyDescriptionList = new ArrayList<>();
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".style-ugc");
+        rows.forEach(row -> {
+            Element titleElement = row;
+            String vacancyDescr = titleElement.text();
+            vacancyDescriptionList.add(vacancyDescr);
+        });
+        for (String str : vacancyDescriptionList) {
+            String str1 = vacancyDescription + String.format("%s%n", str);
+            vacancyDescription = str1;
+        }
+        return vacancyDescription;
+    }
 
-        for (int i = 1; i <= 5; i++) {
+    public static void main(String[] args) throws IOException {
+        HabrCareerParse habrCareerParse = new HabrCareerParse();
+        HabrCareerDateTimeParser habrCareerDateTimeParser = new HabrCareerDateTimeParser();
+        ArrayList<Post> listPost = new ArrayList<>();
+        for (int i = 1; i <= 1; i++) {
             Connection connection = Jsoup.connect(PAGE_LINK + i);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
@@ -27,8 +48,16 @@ public class HabrCareerParse {
                 Element titleElementDate = row.select(".vacancy-card__date").first();
                 Element linkElementDate = titleElementDate.child(0);
                 String date = String.format(linkElementDate.attr("datetime"));
-                System.out.printf("%s %s %s%n", vacancyName, link, date);
+                try {
+                    String descr = habrCareerParse.retrieveDescription(link);
+                    Post post = new Post(vacancyName, link, descr, habrCareerDateTimeParser.parse(date));
+                    listPost.add(post);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             });
         }
+        System.out.println(listPost.get(0));
+        System.out.println(listPost.size());
     }
 }
