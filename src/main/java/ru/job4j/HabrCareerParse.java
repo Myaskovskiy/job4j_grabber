7ffc717b58ff57f28539c4a84001b402a2057d3e
vmyaskovskiy <slavav1977@yandex.ru>
2022-09-14 16:18:13 +0300

    2.4. HabrCareerParse [#285213] new

--- src/main/java/ru/job4j/HabrCareerParse.java
+++ src/main/java/ru/job4j/HabrCareerParse.java
@@ -15,46 +15,58 @@ public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
     private final DateTimeParser dateTimeParser;
+    private static final int COUNT_PAGE = 1;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
     }
 
-    private String retrieveDescription(String link) throws IOException {
+    private String retrieveDescription(String link) {
         Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
+        Document document = null;
+        try {
+            document = connection.get();
+        } catch (IllegalArgumentException | IOException iae) {
+            iae.printStackTrace();
+        }
         Element description = document.select(".style-ugc").first();
         return description.text();
     }
 
+    private Post post(Element row) {
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        String vacancyName = titleElement.text();
+        String linkVacancy = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        Element titleElementDate = row.select(".vacancy-card__date").first();
+        Element linkElementDate = titleElementDate.child(0);
+        String date = String.format(linkElementDate.attr("datetime"));
+        String descr = retrieveDescription(linkVacancy);
+        Post post = new Post(vacancyName, linkVacancy, descr, this.dateTimeParser.parse(date));
+        return post;
+    }
+
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> listPost = new ArrayList<>();
-        for (int i = 1; i <= 1; i++) {
+        for (int i = 1; i <= COUNT_PAGE; i++) {
             Connection connection = Jsoup.connect(link + i);
-            Document document = connection.get();
+            Document document = null;
+            try {
+                document = connection.get();
+            } catch (IllegalArgumentException | IOException iae) {
+                iae.printStackTrace();
+            }
             Elements rows = document.select(".vacancy-card__inner");
             for (Element row : rows) {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                String linkVacancy = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                Element titleElementDate = row.select(".vacancy-card__date").first();
-                Element linkElementDate = titleElementDate.child(0);
-                String date = String.format(linkElementDate.attr("datetime"));
-                try {
-                    String descr = retrieveDescription(linkVacancy);
-                    Post post = new Post(vacancyName, linkVacancy, descr, this.dateTimeParser.parse(date));
-                    listPost.add(post);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                Post post = post(row);
+                listPost.add(post);
             }
         }
         return listPost;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         HabrCareerDateTimeParser habrCareerDateTimeParser = new HabrCareerDateTimeParser();
         HabrCareerParse habrCareerParse = new HabrCareerParse(habrCareerDateTimeParser);
         String link = PAGE_LINK;
