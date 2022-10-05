4cec3089cde382fc0036358789bd8a24e8b7296b
vmyaskovskiy <slavav1977@yandex.ru>
2022-10-05 10:30:23 +0300

    5. PsqlStore [#285209] new3

--- src/main/java/ru/job4j/PsqlStore.java
+++ src/main/java/ru/job4j/PsqlStore.java
@@ -1,5 +1,6 @@
 package ru.job4j;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.time.LocalDateTime;
@@ -105,19 +106,23 @@ public class PsqlStore implements Store, AutoCloseable {
         return properties;
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         Properties pr = getProperties();
-        PsqlStore psqlStore = new PsqlStore(pr);
-        LocalDateTime.parse("2022-09-28T12:06:56");
         Post post = new Post("Системный аналитик (Микросервисы)",
                 "https://career.habr.com/vacancies/1000111462",
                 "ЕМ ПРЕДСТОИТ ЗАНИМАТЬСЯ",
                 LocalDateTime.parse("2022-09-28T12:06:56")
         );
-        psqlStore.save(post);
-        List<Post> list = psqlStore.getAll();
-        System.out.println(list.get(0));
-        Post post1 = psqlStore.findById(1);
-        System.out.println(post1);
+
+        try (PsqlStore psqlStore = new PsqlStore(pr)) {
+            psqlStore.save(post);
+            List<Post> list = psqlStore.getAll();
+            System.out.println(list.get(0));
+            Post post1 = psqlStore.findById(1);
+            System.out.println(post1);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
